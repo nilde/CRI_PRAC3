5b2bfb0c649e0d73ac601171576ca3fc2997d404
jordigc2 <jordigc2@gmail.com>
2017-11-22 19:14:48 +0100

    Split data
    Split data entre train y test con un posible ratio

--- Reader.py
+++ Reader.py
@@ -2,6 +2,17 @@ import csv
 import numpy as np
 import operator
 from collections import OrderedDict
+
+def split_data(data, train_ratio=0.8):
+    indices = np.arange(data.shape[0])
+    np.random.shuffle(indices)
+    n_train = int(np.floor(data.shape[0]*train_ratio))
+    indices_train = indices[:n_train]
+    indices_val = indices[n_train:]
+    train = data[indices_train, :]
+    validation = data[indices_val, :]
+    return train, validation
+
 destFile='shortDatabase.csv'
 dictionaryPositives={}
 dictionaryNegatives={}
@@ -23,10 +34,17 @@ with open(destFile,'r') as destF:
     data = [data for data in dataForIter]
 data_array = np.asarray(data) 
 
+data_array = np.asarray(data) 
+
+data_tr,data_tst = split_data(data_array)
+print len(data_array)
+print len(data_tr)
+print len(data_tst)
+
 #Some treaatment to create train and test sets
 
 #Generation of the dicts
-for eachEntry in data_array:
+for eachEntry in data_tr:
 	if eachEntry[-1]=='0':
 		for eachWord in eachEntry[1].split(' '):
 			if (dictionaryNegatives.has_key(eachWord)):
